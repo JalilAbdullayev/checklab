0a972c6aca7dd99001cedfc1eaa17cdd815c8ab6
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-04 15:40:03 +0400

    Update assets

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
+                'public/back/node_modules/morrisjs/morris.css',
+                'public/back/node_modules/toast-master/css/jquery.toast.css',
+                'public/back/dist/css/style.min.css',
+                'public/back/dist/css/pages/dashboard1.css',
+                'public/front/swiper/swiper.min.css',
+                'public/front/bootstrap/css/bootstrap.min.css',
+                'public/front/css/reset.css',
+                'public/front/css/global.css',
+                'public/front/css/main.css',
+                'public/front/css/mobile.css'
             ],
             refresh: true,
         }),
